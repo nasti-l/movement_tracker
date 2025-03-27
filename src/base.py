d0c25abb2d733cd6f235e54986294eb39d564b0d
anastasia <anastasia.lurye@lean-ai-tech.com>
2025-03-27 13:37:08 +0200

    threaded component added

--- src/base.py
+++ src/base.py
@@ -1,3 +1,4 @@
+import threading
 from abc import ABC, abstractmethod
 from typing import Callable
 import queue
@@ -6,9 +7,11 @@ import numpy as np
 class VideoSourceBase(ABC):
     def __init__(self, callback: Callable[[np.ndarray], None]):
         self._callback = callback
+
     @abstractmethod
     def start(self):
         pass
+
     @abstractmethod
     def stop(self):
         pass
@@ -17,7 +20,25 @@ class DataProcessorBase(ABC):
     def __init__(self, output_queue: queue.Queue):
         self._output_queue = output_queue
 
+    @abstractmethod
     def process_frame(self, frame: np.ndarray):
+        pass
+
+class ThreadedComponentBase(ABC):
+    def __init__(self):
+        self.running = False
+        self._thread = threading.Thread(target=self._run_loop, daemon=True)
+
+    def start(self):
+        self.running = True
+        self._thread.start()
+
+    def stop(self):
+        self.running = False
+        self._thread.join()
+
+    @abstractmethod
+    def _run_loop(self):
         pass
 
 class StorageBase(ABC):
